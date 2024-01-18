3a28ae1b7e494fae57b72b9b97c8c766a9412e58
Aleexm7 <alejandromunozdominguez@gmail.com>
2024-01-18 12:52:25 +0100

    solucion de errores crud

--- resources/views/products/viewproducts.blade.php
+++ resources/views/products/viewproducts.blade.php
@@ -4,22 +4,21 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="{{URL::asset('assets/img/favicon.png') }}">
   <title>
     Panel administrador
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
   <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{URL::asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{URL::asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <!-- Material Icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
   <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
+  <link id="pagestyle" href="{{URL::asset('assets/css/material-dashboard.css?v=3.1.0') }}" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
 
   <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
